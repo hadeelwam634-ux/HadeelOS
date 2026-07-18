c370dd123197631accd02309eac2c4e729d33420
hadeelwam634-ux <hadeelwam634@gmail.com>
2026-07-18 07:47:00 +0300

    Add files via upload

--- decision-engine/src/knowledge-graph/index.ts
+++ decision-engine/src/knowledge-graph/index.ts
@@ -3,3 +3,4 @@ export * from "./InMemoryKnowledgeGraphRepository";
 export * from "./KnowledgeGraphService";
 export * from "./CausalMaturityPolicy";
 export * from "./errors";
+export * from "./PostgresKnowledgeGraphRepository";
